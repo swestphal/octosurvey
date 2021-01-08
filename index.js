495a2af822188fe26185e4e9bb2799d7bfd9e818
Simone Westphal <sw@simonewestphal.de>
2021-01-08 11:38:14 +0100

    tests: Test heroku build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'hhf' });
+    res.send({ hi: 'simone' });
 });
 
 const PORT = process.env.PORT || 5000;
