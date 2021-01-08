fb82306086bd913c485c72673cce369a90adb385
Simone Westphal <sw@simonewestphal.de>
2021-01-08 15:44:50 +0100

    feature: Add google authentication

---
+++ config/passportKeys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID:
+        '413876111185-i8915chpac6b6kqjk0fk935g49rd6ap7.apps.googleusercontent.com',
+    googleClientSecret: 'sJQP0qt86DZafGc3ZwK55pCQ',
+};
--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStategy = require('passport-google-oauth20').Strategy;
+const passportKeys = require('./config/passportKeys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'simone' });
-});
+passport.use(
+    new GoogleStategy(
+        {
+            clientID: passportKeys.googleClientID,
+            clientSecret: passportKeys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log('access token', accessToken);
+            console.log('refresh token', refreshToken);
+            console.log('profile token', profile);
+        }
+    )
+);
 
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email'],
+    })
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email'],
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
