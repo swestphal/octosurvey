63704d3634f7d4179383a98590cc16a1f16c1793
Simone Westphal <sw@simonewestphal.de>
2021-01-09 19:01:39 +0100

    core: Add material-ui

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+const App = () => {
+    return (
+        <Button variant='contained' color='primary'>
+            <h1>hello world!</h1>
+        </Button>
+    );
+};
+
+export default App;
